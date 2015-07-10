0d0c544f489e7a6d99204ba3a182ccd1f71748d1
marek <cos>
2015-07-10 10:00:00 +0200

    testy pobieranie z pliku

--- src/Tests/ValidationTest.java
+++ src/Tests/ValidationTest.java
@@ -2,8 +2,15 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -33,21 +40,40 @@ public class ValidationTest {
 	}
 
 	@Test
-	public void test2() throws IOException, WrongWhileException {
-/*		String input = "process_id=_featureManager.getProcessInstanceFeature().getWFLIProcessId(); \n"
-				+ "if ((_COUNTRY_ID.getValue()==320)||(_COUNTRY_ID.getValue()==390)) \n" + "{\n"
-				+ "	dbPort = _featureManager.getTytanDBPortFeature(); \n"
-				+ "	query = dbPort.executeStatement(\"select NOORDER, RESITEM_ID from table (xpwflcitem.fcGetMSISDNrow(\"+process_id+\"))\"); \n"
-				+ "	while(query.next()) \n" + "	{ \n"
-				+ "		noorder = java.math.BigInteger(query.getString(\"NOORDER\")); \n"
-				+ "		resitem = java.math.BigInteger(query.getString(\"RESITEM_ID\")); \n"
-				+ "		_TABLE.getRow(noorder).getParameter(5).setValue(resitem); \n" + "	} \n" + "}\n";
-		ExpressionParser parser = new ExpressionParser();
-		List<Expression> list = parser.parse(input);
+	public void testParser() throws IOException, WrongWhileException {
+		String input = "";
+		String line;
+		try {
+			FileReader file = new FileReader("daneDoTestow.txt");
+			BufferedReader bufferedReader = new BufferedReader(file);
+			while ((line = bufferedReader.readLine()) != null){
+				input += line + "\n";
+			}
+			bufferedReader.close();
+		} catch (FileNotFoundException e) {
 
-		assertSame(3, list.size());
-		assertTrue(list.get(1) instanceof If);
-		// assertTrue(list.get(1) instanceof Invocation); */
+		}
+
+		List<String> test=Arrays.asList(input.split("\n"));
+		assertSame(5, test.size());
+		/*
+		 * String input =
+		 * "process_id=_featureManager.getProcessInstanceFeature().getWFLIProcessId(); \n"
+		 * +
+		 * "if ((_COUNTRY_ID.getValue()==320)||(_COUNTRY_ID.getValue()==390)) \n"
+		 * + "{\n" + "	dbPort = _featureManager.getTytanDBPortFeature(); \n" +
+		 * "	query = dbPort.executeStatement(\"select NOORDER, RESITEM_ID from table (xpwflcitem.fcGetMSISDNrow(\"+process_id+\"))\"); \n"
+		 * + "	while(query.next()) \n" + "	{ \n" +
+		 * "		noorder = java.math.BigInteger(query.getString(\"NOORDER\")); \n"
+		 * +
+		 * "		resitem = java.math.BigInteger(query.getString(\"RESITEM_ID\")); \n"
+		 * +
+		 * "		_TABLE.getRow(noorder).getParameter(5).setValue(resitem); \n"
+		 * + "	} \n" + "}\n"; ExpressionParser parser = new ExpressionParser();
+		 * List<Expression> list = parser.parse(input);
+		 * 
+		 * assertSame(3, list.size()); assertTrue(list.get(1) instanceof If);
+		 */
 	}
 
 	@Test
