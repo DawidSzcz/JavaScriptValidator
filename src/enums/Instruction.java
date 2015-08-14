684227b1d4e4046a1c4a24c4117d8e028598975a
Marek Zydorczak <marek160891@o2.pl>
2015-08-14 11:59:45 +0200

    take language in combobox

--- src/enums/Instruction.java
+++ src/enums/Instruction.java
@@ -2,15 +2,15 @@ package enums;
 
 public enum Instruction {
 	
-	TRY("^\\s*try\\W"),
-	ELSE("^\\s*else\\W"),
-	IF("^\\s*if\\s*\\("),
-	SWITCH("^\\s*([\\w$_]+:\\s*)?switch\\s*\\("),
-	FOR("^\\s*([\\w$_]+:\\s*)?for\\s*\\("),
-	WHILE("^\\s*([\\w$_]+:\\s*)?while\\s*\\("),
-	CATCH("^\\s*catch\\s*\\("),
-	FUNCITON("^\\s*function\\s+[_\\$a-zA-Z]+[_\\$\\w]*\\s*\\("),
-	VAR("^\\s*var\\W"),
+	TRY("try"),
+	ELSE("else"),
+	IF("if\\s*\\("),
+	SWITCH("([\\w$_]+:\\s*)?switch\\s*\\("),
+	FOR("([\\w$_]+:\\s*)?for\\s*\\("),
+	WHILE("([\\w$_]+:\\s*)?while\\s*\\("),
+	CATCH("catch\\s*\\("),
+	VAR("(^|\\s+)var\\s+"),
+	FUNCITON("function\\s+[_\\$a-zA-Z]+[_\\$\\w]*\\s*\\("),
 	PROGRAM("");
 	
 	public final String content;
